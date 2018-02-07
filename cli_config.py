bbe0aa114f792f7296377606357d23552933ef2d
rbn920 <robertb.nelson@gmail.com>
2018-02-07 12:57:28 -0800

    Create functioning Config class

--- cli_config.py
+++ cli_config.py
@@ -1,84 +1,64 @@
 import click
 import configparser
 import privy
-from getpass import getpass
 
 
 class Config:
-    def __init__(self):
-        ready = self._load()
-
-        if not ready:
-            print('There are no configured exchanges')
-
-        elif ready:
-            self.unlock()
+    def __init__(self, password):
+        self._load()
+        self.password = password.encode()
 
     def _load(self):
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
-        ready = self.config.has_section('creds')
+        self.existing = self.config.has_section('keys')
+        if self.existing:
+            self.keys = {k: self.config['keys'][k] for
+                         k in self.config['keys']}
+            self.secrets = {k: self.config['secrets'][k] for
+                            k in self.config['secrets']}
+        else:
+            self.keys = {}
+            self.secrets = {}
 
-        self.key = None
-        self.secret = None
+    def _encrypt(self, key, secret):
+        return [privy.hide(key.encode(), self.password, security=5),
+                privy.hide(secret.encode(), self.password, security=5)]
 
-        return ready
+    def _decrypt(self, key, secret):
+        return [privy.peek(key, self.password, security=5),
+                privy.peek(secret, self.password, security=5)]
 
-    def ready(self):
-        return self.key is not None and self.secret is not None
+    def add_password(self, password):
+        pass
 
-    def configure(self):
-        print('-- API Configuration --')
-
-        check = False
-        password = getpass('Enter your password: ')
-        while check != password:
-            check = getpass('Re-enter your password: ')
-            if check == password:
-                break
-
-        password = password.encode()
-        key = input('API Key: ').strip().encode()
-        secret = input('API Secret: ').strip().encode()
-
-        self.key = key
-        self.secret = secret
-
-        print('Encrypting your credentials...')
-        key = privy.hide(key, password, security=5)
-        secret = privy.hide(secret, secret, security=5)
-
-        creds = {'key': key, 'secret': secret}
-        self.config['creds'] = creds
+    def add_keys(self, name, key, secret):
+        enc_key, enc_secret = self._encrypt(key, secret)
+        self.keys[name] = enc_key
+        self.secrets[name] = enc_secret
+        self.config['keys'] = self.keys
+        self.config['secrets'] = self.secrets
+        print('Encrypting your credential...')
         self.config.write(open('config.ini', 'w'))
-
-    def unlock(self):
-        password = getpass('Enter your password: ')
-        password = password.encode()
-        print('Unlocking encrypted credentials...')
-
-        try:
-            self.key = privy.peek(self.config['creds']['key'], password)
-            self.secret = privy.peek(self.config['creds']['secret'], password)
-        except ValueError:
-            print('Incorrect Password!')
-            exit(1)
 
 
 pass_config = click.make_pass_decorator(Config)
 
 
 @click.group()
+@click.password_option()
 @click.pass_context
-def cli(ctx):
-    ctx.obj = Config()
+def cli(ctx, password):
+    ctx.obj = Config(password)
 
 
 @cli.command()
-@click.argument('name')
+@click.argument('name', nargs=1)
+@click.argument('key', nargs=1)
+@click.argument('secret', nargs=1)
 @pass_config
-def add_exchange(config, name):
-    print(name)
+def add_exchange(config, name, key, secret):
+    config.add_keys(name, key, secret)
 
 
 if __name__ == '__main__':
